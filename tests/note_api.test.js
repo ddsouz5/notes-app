c0246184cbde21688d57c2d644c0b3e7d13c6b1f
ddsouz5 <ddsouz5@gmail.com>
2024-05-28 21:22:01 -0400

    initialize database before every test

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,18 +1,55 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
+const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
+const Note = require('../models/note')
+
+const initialNotes = [
+  {
+    content: 'HTML is easy',
+    important: false
+  },
+  {
+    content: 'Browser can execute only Javascript',
+    important: 'true'
+  }
+]
+
+beforeEach(async () => {
+  await Note.deleteMany({})
+  let noteObject = new Note(initialNotes[0])
+  await noteObject.save()
+  noteObject = new Note(initialNotes[1])
+  await noteObject.save()
+})
 
 test('notes are returned as json', async () => {
   await api
     .get('/api/notes')
     .expect(200)
-    .expect('Content-Type', '/application\/json')
+    .expect('Content-Type', /application\/json/)
+})
+
+test('there are two notes', async () => {
+  const response = await api.get('/api/notes')
+// execution gets here only after the HTTP request is complete
+// the result of HTTP request is saved in variable response
+  assert.strictEqual(response.body.length, initialNotes.length)
+})
+
+test('the first note is about HTTP methods', async () => {
+  const response = await api.get('/api/notes')
+
+  const contents = response.body.map(e => e.content)
+  //is the argument truthy  
+  assert(contents.includes('HTML is easy'))
 })
 
 after(async () => {
   await mongoose.connection.close()
 })
+
